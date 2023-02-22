613bc07f0edea5f5cc748731ca674327b60b3779
mohammedfalaah <falahmkba@gmail.com>
2023-02-23 02:35:03 +0530

    project version 1

---
+++ main.js
@@ -0,0 +1,9 @@
+// Hero Swiper Slider
+var swiper = new Swiper('.swiper-container', {
+  direction : 'vertical',
+  spaceBetween: 0, 
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
